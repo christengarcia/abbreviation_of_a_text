12667fe3a4138a3a95eca4bec0dff15a58507fb2
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-27 15:25:37 +1100

    Initial commit

---
+++ abbreviation_of_a_text/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * C# Program to Display the Abbreviation of a Text
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Abbreviation
+{
+    string str;
+    public void Readdata()
+    {
+        Console.WriteLine("Enter a String :");
+        str = Console.In.ReadLine();
+    }
+}
